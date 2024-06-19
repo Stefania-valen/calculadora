f10e0776349f5281a3aa445caee7ba65c7de4f41
Stefania-valen <stef.valenzuela@duocuc.cl>
2024-06-19 15:49:56 -0400

    Update calculadora.py
    op nueva (5)

--- calculadora.py
+++ calculadora.py
@@ -16,25 +16,29 @@ def division (num1,num2):
         resultado = num1/num2
         return resultado
     
-
-while True:
-    numero1 = int(input("Ingrese el primer numero: "));
-    op = int(input("Ingrese la accion que desea realizar \n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir\n "));
-    numero2 = int(input("Ingrese el segundo numero: "));
-    if op == 1:
-        print(sumar(numero1,numero2))
-    if op == 2:
-        print(restar(numero1,numero2))
-    if op == 3:
-        print(multiplicar(numero1,numero2))
-    if op == 4:
-        try:
-            print(division(numero1,numero2))
-        except ZeroDivisionError as e:
-            print("no se púede dividir entre 0");
-    menu = str(input("¿Desea continuar?(S/N)"))
-    if menu.upper() == "S":
+op = 0
+while op != 5:
+    try:
+        numero1 = int(input("Ingrese el primer numero: "));
+        op = int(input("Ingrese la accion que desea realizar \n 1. Sumar \n 2. Restar \n 3. Multiplicar \n 4. Dividir\n 5. Salir \n"));
+        numero2 = int(input("Ingrese el segundo numero: "));
+        if op == 1:
+            print(sumar(numero1,numero2))
+        if op == 2:
+            print(restar(numero1,numero2))
+        if op == 3:
+            print(multiplicar(numero1,numero2))
+        if op == 4:
+            try:
+                print(division(numero1,numero2))
+            except ZeroDivisionError as e:
+                print("no se púede dividir entre 0");
+        menu = str(input("¿Desea continuar?(S/N)"))
+        if menu.upper() == "S":
+            continue
+        else:
+            break
+    except ValueError as a:
+        print("Ingrese un valor correcto")
         continue
-    else:
-        break
 print("Adios")
