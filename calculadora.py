3c93df2468d29431c1fbe182d74bde7996695201
bastian-alvarez <ba.alvarezb@duocuc.cl>
2024-06-19 15:16:55 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,6 @@
 # este es el archivo python en el cual mis compañeros y yo trabajaremos para 
 # crear una calculadora de manera colaborativa
+
+def sumar(num1,num2):
+    resultado=num1+num2
+    return resultado
